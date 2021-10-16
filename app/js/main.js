7be7e78534203e6ff673deeb0f213b27f8f6da32
diahamada <diahamada@gmail.com>
2021-10-16 17:13:32 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.slider__inner').slick({
+        dots: true,
+        arrows: false,
+       
+    });
+    var mixer = mixitup('.gallery__content');
+    var mixer = mixitup('.commercial__inner');
+});
